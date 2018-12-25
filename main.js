09e94aa5ccc69a039e58cda309820da9f28e61a0
Ryel Oliver <rmuchunku@gmail.com>
2018-12-25 01:20:50 +0000

    Add draggable ordering

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ const ToDo = Backbone.Model.extend({
   },
 
   toggle: function () {
-    this.set('done', !this.get('done'), { validate: true })
+    this.set('done', !this.get('done'))
   },
 
   editDescription: function (description) {
-    this.set('description', description, { validate: true })
+    this.set('description', description)
   },
 
   validate: function (attributes) {
-    const { id, description, done } = attributes
+    const { id, description, done, order } = attributes
 
     if (id === undefined) {
       throw Error('ID is required.')
@@ -23,14 +23,21 @@ const ToDo = Backbone.Model.extend({
     if (done === undefined) {
       throw Error('Done is required.')
     }
+    if (order === undefined) {
+      throw Error('Order is required.')
+    }
   }
 })
 
 const ToDoList = Backbone.Collection.extend({
   model: ToDo,
+  comparator: 'order',
 
   nextId: function () {
-    return this.length ? this.last().get('id') + 1 : 1
+    return this.length ? this.reduce((maxId, toDo) => {
+      const id = toDo.get('id')
+      return id > maxId ? id : maxId
+    }, 1) + 1 : 1
   }
 })
 
@@ -55,16 +62,44 @@ const IconView = Backbone.View.extend({
   }
 })
 
+const ToDoPlaceholderView = Backbone.View.extend({
+  tagName: 'li',
+  className: 'placeholder',
+
+  initialize: function (props) {
+    this.props = props
+  },
+
+  render: function () {
+    const { description, done } = this.props
+    this.$el.html(`
+      <div class="${done ? 'view done' : 'view'}">
+        <input type="checkbox" ${done ? 'checked' : ''}/>
+        <label>${description}</label>
+      </div>
+    `)
+    return this
+  }
+})
+
 const ToDoView = Backbone.View.extend({
   tagName: 'li',
   className: 'to-do',
+  attributes: {
+    draggable: true
+  },
   
   events: {
     'click .view input': 'toggle',
     'click .view label': 'startEdit',
     'change .edit input': 'edit',
     'blur .edit input': 'endEdit',
-    'click .remove': 'remove'
+    'click .remove': 'destroy'
+  },
+
+  initialize: function () {
+    this.listenTo(this.model, 'change', this.render)
+    this.listenTo(this.model, 'destroy', this.removed)
   },
 
   toggle: function() {
@@ -96,12 +131,12 @@ const ToDoView = Backbone.View.extend({
     }
   },
 
-  remove: function() {
+  destroy: function() {
     this.model.trigger('destroy', this.model)
   },
 
-  initialize: function () {
-    this.listenTo(this.model, 'change', this.render)
+  removed: function() {
+    this.remove()
   },
 
   render: function () {
@@ -135,25 +170,53 @@ const ToDoListView = Backbone.View.extend({
   className: 'to-do-list',
   
   events: {
-    'click .add textarea': 'startAdd',
-    'keydown .add textarea': 'add',
-    'blur .add textarea': 'endAdd',
-    'click .all': 'toggleList',
-    'click .remove-done': 'removeDone'
+    'click .add textarea': 'onClickAdd',
+    'keydown .add textarea': 'onKeyDownAdd',
+    'blur .add textarea': 'onBlurAdd',
+    'click .all': 'onClickAll',
+    'click .remove-done': 'onClickRemoveDone',
+    'dragstart .to-do': 'onDragStart',
+    'dragenter .to-do': 'onDragEnter',
+    'dragover li': 'onDragOver',
+    'dragend .to-do': 'onDragEnd',
+    'drop li': 'onDrop'
   },
   
-  startAdd: function () {
+  initialize: function () {
+    this.listenTo(this.collection, 'add', this.added)
+    this.listenTo(this.collection, 'change:done', this.changeDone)
+    this.listenTo(this.collection, 'remove', this.removed)
+    this.listenTo(this.collection, 'reset', this.reset)
+  },
+
+  getListItem: function (element) {
+    let toDo = element
+    while (toDo.tagName !== 'LI') {
+      toDo = toDo.parentElement
+    }
+    return toDo
+  },
+
+  removePlaceholder: function () {
+    const placeholder = this.$('.placeholder')
+    if (placeholder) {
+      placeholder.remove()
+    }
+  },
+
+  onClickAdd: function () {
     this.$el.addClass('adding')
   },
 
-  add: function(event) {
+  onKeyDownAdd: function(event) {
     if (event.key != 'Enter' || event.target.value.trim() === '') {
       return
     }
 
     const toDo = new ToDo({
       id: this.collection.nextId(),
-      description: event.target.value
+      description: event.target.value,
+      order: this.collection.length ? this.collection.last().get('order') + 1 : 1
     })
     this.collection.add(toDo)
     
@@ -161,48 +224,119 @@ const ToDoListView = Backbone.View.extend({
     event.preventDefault()
   },
   
-  endAdd: function (event) {
+  onBlurAdd: function (event) {
     if (event.target.value.trim() === '') {
       event.target.value = ''
     }
     this.$el.removeClass('adding')
   },
     
+  onClickAll: function() {
+    const { done, total } = this.count
+    const minimumToggle = done / total < 0.5
+    this.collection.map((toDo) => {
+      toDo.set('done', minimumToggle)
+    })
+  },
+    
+  onClickRemoveDone: function() {
+    this.collection.reset(this.collection.where({ done: false }))
+  },
+
+	onDragStart: function (event) {
+    const toDoElement = this.getListItem(event.target)
+    const toDoId = parseInt(toDoElement.querySelector('.view input').id.match(/toDo(\d*?)Done/)[1])
+    event.originalEvent.dataTransfer.setData('text/plain', toDoId)
+    event.originalEvent.dataTransfer.setData(toDoId, 'id') // Hack to make the ID property accessible in the onDragEnter method
+	},
+  
+	onDragEnter: function (event) {    
+    const toDoElement = this.getListItem(event.target)
+    const list = toDoElement.parentElement
+
+    const movedToDoId = parseInt(event.originalEvent.dataTransfer.types[1])
+    if (movedToDoId === parseInt(toDoElement.querySelector('.view input').id.match(/toDo(\d*?)Done/)[1])) {
+      return
+    }
+    const movedToDo = this.collection.get(movedToDoId)
+
+    const toDoPlaceholder = new ToDoPlaceholderView({
+      description: movedToDo.get('description'),
+      done: movedToDo.get('done')
+    })
+
+    this.removePlaceholder()
+    list.insertBefore(toDoPlaceholder.render().el, toDoElement)
+  },
+  
+  onDragOver: function (event) {
+		event.preventDefault()
+    event.originalEvent.dataTransfer.dropEffect = 'move'
+	},
+
+	onDragEnd: function (event) {
+    this.removePlaceholder()
+  },
+  
+	onDrop: function (event) {
+    const movedToDoId = parseInt(event.originalEvent.dataTransfer.getData('text/plain'))
+    const movedToDo = this.collection.get(movedToDoId)
+    const movedToDoOrder = movedToDo.get('order')
+
+    const targetElement = this.getListItem(event.target)
+    const movedBeforeToDoElement = targetElement.classList.contains('to-do') ? targetElement : targetElement.nextSibling
+    const movedBeforeToDoId = parseInt(movedBeforeToDoElement.querySelector('.view input').id.match(/toDo(\d*?)Done/)[1])
+    const movedBeforeToDo = this.collection.get(movedBeforeToDoId)
+    const movedBeforeToDoOrder = movedBeforeToDo.get('order')
+
+    if (movedToDoOrder === movedBeforeToDoOrder) {
+      return
+    }
+
+    const list = movedBeforeToDoElement.parentElement
+
+    const prioritised = movedToDoOrder > movedBeforeToDoOrder
+    this.collection.each((toDo) => {
+      const order = toDo.get('order')
+      if (prioritised) {
+        if (order >= movedBeforeToDoOrder && order < movedToDoOrder) {
+          toDo.set('order', order + 1)
+        }
+      } else {
+        if (order <= movedBeforeToDoOrder && order > movedToDoOrder) {
+          toDo.set('order', order - 1)
+        }
+      }
+    })
+    movedToDo.set('order', movedBeforeToDoOrder)
+    this.collection.sort()
+
+    const movedToDoElement = this.getListItem(this.$(`#toDo${movedToDoId}Done`)[0])
+    movedToDoElement.remove()
+    this.removePlaceholder()
+
+    const toDoView = new ToDoView({
+      model: movedToDo
+    })
+    list.insertBefore(toDoView.render().el, movedBeforeToDoElement)
+	},
+    
   added: function(toDo) {
     this.renderItem(toDo)
-    this.renderSummary()
-  },
-
-  remove: function (toDo) {
-    this.collection.remove(toDo)
-  },
-
-  removed: function (toDo) {
-    this.renderList()
     this.renderSummary()
   },
     
   changeDone: function() {
     this.renderSummary()
   },
-    
-  toggleList: function() {
-    const { done, total } = this.count
-    const minimumToggle = done / total < 0.5
-    this.collection.map((toDo) => {
-      toDo.set('done', minimumToggle, { validate: true })
-    })
-  },
-    
-  removeDone: function() {
-    this.collection.reset(this.collection.where({ done: false }))
+
+  removed: function (toDo) {
+    this.renderSummary()
   },
 
-  initialize: function () {
-    this.listenTo(this.collection, 'add', this.added)
-    this.listenTo(this.collection, 'change:done', this.changeDone)
-    this.listenTo(this.collection, 'remove', this.removed)
-    this.listenTo(this.collection, 'reset', this.removed)
+  reset: function () {
+    this.renderList()
+    this.renderSummary()
   },
 
   renderItem: function (toDo) {
@@ -274,17 +408,25 @@ const toDoListView = new ToDoListView({
 
 document.querySelector('#toDoList').append(toDoListView.render().el)
 
-toDoList.add([
+toDoList.reset([
   new ToDo({
     id: 1,
-    description: 'One, two'
+    description: 'One, two',
+    order: 1
   }),
   new ToDo({
     id: 2,
-    description: 'Skip a few'
+    description: 'Skip a few',
+    order: 2
   }),
   new ToDo({
     id: 3,
-    description: 'Banana'
+    description: 'Banana',
+    order: 4
+  }),
+  new ToDo({
+    id: 4,
+    description: 'Ninety nine',
+    order: 3
   })
 ])
